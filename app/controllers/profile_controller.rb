58493fb7f3d0df6b0a22d2f502bfd8853b9fc0ca
JinDamanee <88868930+JinDamanee2544@users.noreply.github.com>
2022-12-14 04:00:28 +0700

    Feat: add change password test

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -9,14 +9,12 @@ class ProfileController < ApplicationController
     confirm_password = params[:confirm_password]
     old_password = params[:old_password]
 
-    puts '---------------------- test ----------------------'
-
     if(user.authenticate(old_password)==false)
       redirect_to profile_path, notice: "Wrong old password"
     elsif(new_password != confirm_password)
       redirect_to profile_path, notice: "New password and confirm password do not match"
     else
-      user.update(password: new_password,lock_version: user.lock_version)
+      user.update(password: new_password)
       reset_session
       redirect_to login_path, notice: "Password changed successfully"
     end
--- test/controllers/profile_controller_test.rb
+++ test/controllers/profile_controller_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class ProfileControllerTest < ActionDispatch::IntegrationTest
   setup do
+    @user = users(:admin)
     email = 'admin@gmail.com'
     password = 'admin'
     post login_control_path, params: { email: email, password: password }
@@ -10,5 +11,42 @@ class ProfileControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get profile_url
     assert_response :success
+  end
+
+  test "Change Password" do
+    old_password = 'admin'
+    new_password = 'admin1234'
+    confirm_password = 'admin1234'
+    post profile_changePassword_path, params: { new_password: new_password, confirm_password: confirm_password, old_password: old_password }
+    assert_redirected_to(login_path)
+    assert_equal("Password changed successfully", flash[:notice])
+    assert User.find(@user.id).authenticate(new_password)
+  end
+
+  test "Change Password with wrong old password" do
+    old_password = 'adminmax'
+    new_password = 'admin1234'
+    confirm_password = 'admin1234'
+    post profile_changePassword_path, params: { new_password: new_password, confirm_password: confirm_password, old_password: old_password }
+    assert_redirected_to(profile_path)
+    assert_equal("Wrong old password", flash[:notice])
+  end
+
+  test "Change Password with wrong confirm password" do
+    old_password = 'admin'
+    new_password = 'admin1234'
+    confirm_password = 'admin12345'
+    post profile_changePassword_path, params: { new_password: new_password, confirm_password: confirm_password, old_password: old_password }
+    assert_redirected_to(profile_path)
+    assert_equal("New password and confirm password do not match", flash[:notice])
+  end
+
+  test "Change Password should increase lock_version" do
+    old_password = 'admin'
+    new_password = 'admin1234'
+    confirm_password = 'admin1234'
+    assert_difference("User.find(@user.id).lock_version",1) do
+      post profile_changePassword_path, params: { new_password: new_password, confirm_password: confirm_password, old_password: old_password }
+    end
   end
 end
