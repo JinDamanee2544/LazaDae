cbebb0081694dbf789c7b8ff015c1750c3be869a
nichapanit <nichapanit555@gmail.com>
2022-12-16 23:56:44 +0700

    profile test success

--- test/system/profile_test.rb
+++ test/system/profile_test.rb
@@ -4,18 +4,41 @@ class ProfileTest < ApplicationSystemTestCase
     setup do
         #@user = users(:admin)
         visit '/login'
-        fill_in "Email", with: users(:admin).email
-        fill_in "Password", with: "admin"
-        click_on "login"
-        #assert_selector "h1", text: "Market"
     end
 
-    test "should_show_profile" do
-        visit "/my_market"
+    test "should_show_profile_admin" do
+        fill_in "Email", with: users(:admin).email
+        fill_in "Password", with: "admin"
+        click_on("login")
+        assert_selector "h1", text: "Market"
+        
+        visit "/profile"
+        assert_selector "label", text: "Email"
+        assert_selector "label", text: "Name"
+        assert_selector "label", text: "Role"
+    end
 
-        #assert_selector "label", text: "Email"
-        #assert_selector "label", text: "Name"
-        #assert_selector "label", text: "Role"
-        #click_on "Change Password"
+    test "should_show_profile_seller" do
+        fill_in "Email", with: users(:seller).email
+        fill_in "Password", with: "seller"
+        click_on("login")
+        assert_selector "h1", text: "Seller Guy's Shop Inventory"
+        
+        visit "/profile"
+        assert_selector "label", text: "Email"
+        assert_selector "label", text: "Name"
+        assert_selector "label", text: "Role"
+    end
+
+    test "should_show_profile_buyer" do
+        fill_in "Email", with: users(:buyer).email
+        fill_in "Password", with: "buyer"
+        click_on("login")
+        assert_selector "h1", text: "Market"
+        
+        visit "/profile"
+        assert_selector "label", text: "Email"
+        assert_selector "label", text: "Name"
+        assert_selector "label", text: "Role"
     end
 end
